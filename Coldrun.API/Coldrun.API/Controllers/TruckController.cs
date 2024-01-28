3d92651b1d6e3567827304907878c32c1e5a4d3a
molsz700@wp.pl <molsz700@wp.pl>
2024-01-28 20:59:57 +0100

    add empty project

---
+++ Coldrun.API/Coldrun.API/Controllers/TruckController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coldrun.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TruckController : ControllerBase
+    {
+        
+    }
+}
